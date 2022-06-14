7631a5620d6a46a6bb0dbaf15fcd1f0cc889693a
Tonje <91598255+tonjetj@users.noreply.github.com>
2022-06-15 00:18:17 +0200

    g

--- JS/navOtherPages.js
+++ JS/navOtherPages.js
@@ -1,14 +1,17 @@
 const nav = document.querySelector("nav");
+const breadcrumbs = document.querySelector(".breadcrumbs");
 
 function handleScroll() {
   console.log(window.scrollY);
 
   const scrolledY = window.scrollY;
 
-  if (scrolledY > 0) {
+  if (scrolledY > 20) {
     nav.classList.add("scrolled");
+    breadcrumbs.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
+    breadcrumbs.classList.remove("scrolled");
   }
 }
 
