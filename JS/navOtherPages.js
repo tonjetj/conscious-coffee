79e6296ee9c68bcab8e3ed0e2dd37dc03da08750
Tonje <91598255+tonjetj@users.noreply.github.com>
2022-06-15 05:01:56 +0200

    g

--- JS/navOtherPages.js
+++ JS/navOtherPages.js
@@ -1,7 +1,5 @@
 const nav = document.querySelector("nav");
 const breadcrumbs = document.querySelector(".breadcrumbs");
-const logo = document.querySelector("nav .logo");
-const menuIcon = document.querySelector("nav .menu-icon");
 
 function handleScroll() {
   console.log(window.scrollY);
@@ -10,14 +8,12 @@ function handleScroll() {
 
   if (scrolledY > 20) {
     nav.classList.add("scrolled");
-    logo.classList.add("scrolled");
+
     breadcrumbs.classList.add("scrolled");
-    menuIcon.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
-    logo.classList.remove("scrolled");
+
     breadcrumbs.classList.remove("scrolled");
-    menuIcon.classList.remove("scrolled");
   }
 }
 
